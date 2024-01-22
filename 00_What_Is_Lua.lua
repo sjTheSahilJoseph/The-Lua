a0672c8c1cfe2916f87c4a118cbd3cd2ce311a92
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-01-22 15:30:26 +0500

    What is Lua?

---
+++ 00_What_Is_Lua.lua
@@ -0,0 +1,3 @@
+-- What is Lua?
+-- Lua is a procedural programming language used to develop software, work with machine learning and to create all sorts of games, from a basic text game all the way to VR games.
+
