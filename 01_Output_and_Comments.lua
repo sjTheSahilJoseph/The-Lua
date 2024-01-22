0adb94f670a664808d33b86219631e02872fb596
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-01-22 15:30:35 +0500

    Output and Comments.

---
+++ 01_Output_and_Comments.lua
@@ -0,0 +1,27 @@
+-- For printing to the command line, we can use print() function.
+-- single quotes and double quotes doesn't matter for strings here.
+print("Hello")
+print('hello')
+
+-- It'll add a new line by default.
+
+
+-- Comments.
+-- Single line comment start with --.
+
+-- print hello to the terminal.
+print("hello") -- works fine here as well.
+
+-- Multiline comment starts with --[[ content ]] ends with these.
+
+--[[
+    Comment
+    comment 
+    and comment...
+]]
+
+-- NOTE, there should not be a space between --[[.
+
+
+-- We can do string concatination using .. operator in print function.
+print("hello" .. " " .. "hi")
